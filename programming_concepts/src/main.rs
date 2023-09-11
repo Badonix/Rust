2b20239042cb1cdd5d2112c87c6c2d56c77b2aa2
Nick <nikoloz.danelia@redberry.ge>
2023-09-12 00:10:35 +0400

    data types

--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let x = 5;
+    let x: (i32, f64, u8) = (500, 6.4, 1);
 
-    let x = x + 1;
+    let five_hundred = x.0;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let six_point_four = x.1;
 
-    println!("The value of x is: {x}");
+    let one = x.2;
+
+    println!("{five_hundred} {six_point_four} {one}")
 }
