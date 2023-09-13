c0e263b07e6bddc3e5e53e391572c996dc9ac99f
Nick <nikoloz.danelia@redberry.ge>
2023-09-13 20:34:54 +0400

    n-th fibonacci number

--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,7 +1,25 @@
 fn main() {
     let celsius = farenheit_to_celsius(141.32);
     println!("{celsius}");
+    let nth = nth_fibonacci(6);
+    println!("{}", nth);
 }
 fn farenheit_to_celsius(farenheit: f32) -> f32 {
     (farenheit - 32.0) * (5.0 / 9.0)
+}
+fn nth_fibonacci(n: u32) -> u32 {
+    if n == 1 {
+        1
+    } else if n == 2 {
+        1
+    } else {
+        let mut prev = 1;
+        let mut current = 1;
+        for _ in 2..n {
+            let new_current = prev + current;
+            prev = current;
+            current = new_current;
+        }
+        current
+    }
 }
