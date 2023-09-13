8c700a73d97e0711e2eb51684bafb4a64704d755
Nick <nikoloz.danelia@redberry.ge>
2023-09-13 20:21:27 +0400

    farenheit to celsius function

--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    for number in (1..4).rev() {
-        println!("{number}!");
-    }
-    println!("LIFTOFF!!!");
+    let celsius = farenheit_to_celsius(141.32);
+    println!("{celsius}");
+}
+fn farenheit_to_celsius(farenheit: f32) -> f32 {
+    (farenheit - 32.0) * (5.0 / 9.0)
 }
