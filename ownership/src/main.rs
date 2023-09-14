1dbfed5e25ff85f7d11e8216d6599238134c895c
Nick <nikoloz.danelia@redberry.ge>
2023-09-14 19:18:59 +0400

    length

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    change(&mut s);
+    let word = String::from("asd lalala es aris");
+    let length = first_word(&word);
+    println!("{length}");
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
-    println!("{some_string}");
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
